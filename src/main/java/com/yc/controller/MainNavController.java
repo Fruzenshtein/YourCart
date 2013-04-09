b82ac2b6ad4da9dc77798ac71820413f8f76e714
Alex <alex.fruzenshtein@gmail.com>
2013-04-09 23:09:16 +0300

    First commit

---
+++ src/main/java/com/yc/controller/MainNavController.java
@@ -0,0 +1,20 @@
+package com.yc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MainNavController {
+	
+	@RequestMapping(value="/")
+	public ModelAndView mainPage() {
+		return new ModelAndView("home");
+	}
+	
+	@RequestMapping(value="/index")
+	public ModelAndView mainPageIndex() {
+		return new ModelAndView("home");
+	}
+
+}
---
+++ src/main/java/com/yc/init/WebAppConfig.java
@@ -0,0 +1,41 @@
+package com.yc.init;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.yc")
+@PropertySource("classpath:application.properties")
+public class WebAppConfig {
+	
+	@Resource
+	private Environment env;
+	
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename(env.getRequiredProperty("message.source.basename"));
+		source.setUseCodeAsDefaultMessage(true);
+		return source;
+	}
+
+}
