5760866faf434a4f999c7f75df354b406bbbf0e7
Alex <alex.fruzenshtein@gmail.com>
2013-06-29 14:22:57 +0300

    Shop domain model & repository
    - Added: Shop entity, Shop repository
    - Updated User entity

--- src/main/java/com/yc/dao/ShopRepository.java
+++ src/main/java/com/yc/dao/ShopRepository.java
@@ -1,5 +1,9 @@
 package com.yc.dao;
 
-public class ShopRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yc.model.Shop;
+
+public interface ShopRepository extends JpaRepository<Shop, Integer> {
 
 }
