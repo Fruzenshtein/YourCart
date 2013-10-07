789b3e5a8791ba8ba46d6bce67995ba14a43b62c
Alex <alex.fruzenshtein@gmail.com>
2013-10-07 23:36:59 +0300

    Removing of usless code

--- src/main/java/com/yc/controller/SecurityController.java
+++ src/main/java/com/yc/controller/SecurityController.java
@@ -1,22 +1,12 @@
 package com.yc.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.yc.helper.security.SecurityUserContext;
-import com.yc.service.UserService;
-
 @Controller
 public class SecurityController {
 	
-	@Autowired
-	private UserService userService;
-	
-	@Autowired
-	private SecurityUserContext securityUserContext;
-
 	@RequestMapping(value="/user-login")
 	public ModelAndView loginPage() {
 		return new ModelAndView("security/login-form");
